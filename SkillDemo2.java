8fd4ef823086becbb1d31fd0dcfa473594f63e28
asmarking <amarking@ucsd.edu>
2022-02-04 15:16:33 -0800

    Failed Test and First Commit

---
+++ SkillDemo2.java
@@ -0,0 +1,9 @@
+public class SkillDemo2 {
+    static int subtract(int x,int y){
+        int z = y -x ;
+        return z;
+    }
+    public static void main(String[] args){
+
+    }
+}
---
+++ SkillDemo2Test.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/*
+javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" SkillDemo2.java SkillDemo2.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore SkillDemo2Test
+
+ javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar SkillDemo2Test.java SkillDemo2.java
+ java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore SkillDemo2Test 
+
+*/
+
+public class SkillDemo2Test {
+    @Test
+    public void subtractTest() {
+        assertEquals(2,SkillDemo2.subtract(1,4)) ;
+    }
+}
